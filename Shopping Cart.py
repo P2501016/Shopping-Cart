370318775a04c8c00f504ac366b8bb433c55804a
P2501016 <49679685+P2501016@users.noreply.github.com>
2019-05-14 11:14:08 +0100

    Add files via upload

---
+++ Shopping Cart.py
@@ -0,0 +1,85 @@
+def main ():
+
+    print("Welcome to our store. Press any key to continue")
+
+
+###MEN###
+
+while True:
+
+            clothes= input("Please choose:
+                           1- Men
+                           2- Women
+                           3- Kids
+                           4- Sale")
+            
+            Men= input("1- Men")
+            Women= input ("2-Women")
+            Kids= input ("3- Kids")
+            Sale =input("4-SALE")            
+            
+            
+            if  clothes == ("1") or clothes == ("men") or clothes == ("MEN") or clothes == ("Men"):
+                trousers= input ("Chinos \nSkinnyJeans \nSlim Fit Jeans")
+                chinos= input ("Blue Chinos \nBlack Chinos \nRed Chinos")
+                bluechinos= input ("Blue Chinos")
+                blackchinos= input ("Black Chinos")
+                redchinos= input ("Red Chinos")
+                
+
+
+            elif clothes != ("1") or Men !=("Men") or Men != ("men") or Men!= ("MEN"):
+                print ("Please enter a valid number or number")
+
+
+
+            elif clothes == ("2"):
+
+
+
+
+
+
+
+
+
+                if Women == ("2"):
+            
+                  trousers= input ("Chinos \nSkinnyJeans \nSlim Fit Jeans")
+                  chinos= input ("Blue Chinos \nBlack Chinos \nRed Chinos")
+
+                elif Women != ("2"):
+                            print ("enter a valid number")
+                
+
+
+
+                Men= input ("Blue Chinos \nBlack Chinos \nRed Chinos")
+                if chinos== ("Chinos"):
+
+                    chinos= input ("Blue Chinos \nBlack Chinos \nRed Chinos")
+                    bluechinos= input ("Blue Chinos")
+                    blackchinos= input ("Black Chinos")
+                    redchinos= input ("Red Chinos")
+
+
+                    if chinos != ("Chinos") or chinos != ("chinos"):
+                        print ("Enter a valid name")
+                
+
+
+
+            while True:
+                    if clothes == ("Women") or clothes == ("women") or clothes == ("WOMEN"):
+                        trousers = input ("Skinny \nChinos \nSlim Fit")
+
+                    if clothes != ("Women"):
+                        print ("enter a valid name")
+    
+
+                
+
+                
+
+
+main ()
